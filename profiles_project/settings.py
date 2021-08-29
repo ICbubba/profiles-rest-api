ca2b99a6351460ae7b641486a8042f3eee566a0d
Matt Lincoln <matthewtlincoln@gmail.com>
2021-08-29 00:05:48 -0400

    oopsie

--- profiles_project/settings.py
+++ profiles_project/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'w^*bu%o-y9i3g&=&9_3)7pu90#uyrewxkqx_*)rx-(f%2^=xl1'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = bool(int(os.environ.get('DEBUG', 1))
+DEBUG = bool(int(os.environ.get('DEBUG', 1)))
 
 ALLOWED_HOSTS = []
 
